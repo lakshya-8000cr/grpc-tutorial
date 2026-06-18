ca30763aafccc0d9259a77d9fcb237052a358d09
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-06-18 21:56:58 +0530

    comments

--- Go/client/main.go
+++ Go/client/main.go
@@ -14,26 +14,28 @@ import (
 
 
 func main() {
+
+	//this the part where frontend is talking to backend
 	conn, err := grpc.NewClient(
 		"localhost:50051",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithTransportCredentials(insecure.NewCredentials()), // insecure  = dont use tls security on local
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer conn.Close() // close the connection 
 
-	c := pb.NewGreeterClient(conn)
+	c := pb.NewGreeterClient(conn) // this is object initializing the connection
 
 	
-ctx, cancel := context.WithTimeout(
+ctx, cancel := context.WithTimeout( // if server didnt respond with in 1s second cut of the connection
 	context.Background(),
 	time.Second,
 )
 
 defer cancel()
 
-res, err := c.SayHello(
+res, err := c.SayHello(  // this part which is calling and sending the name like rest 
 	ctx,
 	&pb.HelloRequest{
 		Name: "Lakshya",
--- Go/server/main.go
+++ Go/server/main.go
@@ -16,23 +16,23 @@ type server struct {
 }
 
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	return &pb.HelloResponse{
+	return &pb.HelloResponse{         // this is the sayhello function which we have setup  .helloRequest is setup to get from client
 		Message: "Hello " + req.Name,
 	}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":50051") // server will listen to this port
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	grpcServer := grpc.NewServer()
-	pb.RegisterGreeterServer(grpcServer, &server{})
+	grpcServer := grpc.NewServer()  // new server is created 
+	pb.RegisterGreeterServer(grpcServer, &server{}) // when RegisterGreeterServer is called then use the server created above
 
 	fmt.Println("gRPC server running on :50051")
 
-	err = grpcServer.Serve(lis)
+	err = grpcServer.Serve(lis)  
 	if err != nil {
 		log.Fatal(err)
 	}
